a73842a77e1a93c39462b3fa0777be33abede809
Danila Kritsky <danilakritsky@gmail.com>
2022-03-25 00:26:07 +0300

    Create item loader blueprint

--- scraper/items.py
+++ scraper/items.py
@@ -4,9 +4,28 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Field
+
+def extract_repo_name(repo_url: str) -> str:
+    'Extract repo name from its url.'
+    return repo_url.split('/')[-1]
+
+def remove_whitespace(text: str) -> str:
+    """Removes whitespace from a string."""
+    
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class RepoInfo(scrapy.Item):
+    """Item encapsulating repository data."""
+    account = Field()
+    repo_name = Field(
+        input_processor=extract_repo_name
+    )
+    about = Field()
+    website_link = Field()
+    stars = Field()
+    forks = Field()
+    watching = Field()
+    main_branch_info = Field()
+    release_count = Field()
+    latest_release = Field()
