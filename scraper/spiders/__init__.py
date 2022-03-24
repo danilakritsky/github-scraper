f197366a5da6d20fc53a4cc3db9237829e9638d5
Danila Kritsky <danilakritsky@gmail.com>
2022-03-24 13:24:42 +0000

    Refactor the spider

--- scraper/spiders/__init__.py
+++ scraper/spiders/__init__.py
@@ -29,13 +29,11 @@ class ScraperSpider(scrapy.Spider):
     def parse_repos_page(self, response):
         """Parse repos page to extract links to each repository."""
         repo_urls = response.css('[data-hovercard-type="repository"]::attr(href)').getall()
-        self.logger.info(repo_urls)
         for url in repo_urls:
             yield response.follow(url, callback=self.parse_repo_info)
 
         next_page_url = response.css('a.next_page::attr(href)').get()
         # set DOWNLOAD_DELAY = 0.5 in settings.py to avoid making too many requests (429 response code)
-        self.logger.info(next_page_url)
         if next_page_url:
             yield response.follow(next_page_url, callback=self.parse_repos_page)
 
@@ -55,8 +53,8 @@ class ScraperSpider(scrapy.Spider):
         main_branch_info['commit_count'] = response.css('a[href*="commits"]').css('strong::text').get()
         main_branch_commits_url = response.css('a[href*="commits"]::attr(href)').get()       
         
-        release_count = response.css('a[href$="releases"]').css('span::text').get()
-        releases_url = response.css('a[href$="releases"]::attr(href)').get()
+        release_count = response.css('a[href$="/releases"]').css('span::attr(title)').get()
+        releases_url = response.css('a[href$="/releases"]::attr(href)').get()
         item = {
                 'repo_name': repo_name,
                 'about': about,
@@ -65,7 +63,7 @@ class ScraperSpider(scrapy.Spider):
                 'forks': forks,
                 'watching': watching,
                 'main_branch_info': main_branch_info,
-                'release_count': release_count
+                'release_count': release_count,
         }
 
         yield response.follow(
@@ -73,42 +71,72 @@ class ScraperSpider(scrapy.Spider):
             callback=self.parse_commits_page,
             meta={
                 'item': item,
-                'releases_page': releases_url
+                'releases_url': releases_url
             })  # pass as dict to avoid attaching technical details about the response to meta
 
 
     def parse_commits_page(self, response):
         """Parse the main branch commits page for info on the latest commit."""
         item = response.meta['item']
-        item['main_branch_info']['last_commit_author'] = response.css('a[class*="commit-author"]::text').get() # returns None from time to time
-        item['main_branch_info']['last_commit_time'] = response.css('relative-time::attr(datetime)').get()
-        item['main_branch_info']['last_commit_message'] = response.css('a[data-test-selector="commit-tease-commit-message"]::text').get()
-        yield item
+        item['main_branch_info']['latest_commit_author'] = response.css('a[class*="commit-author"]::text').get() # returns None from time to time
+        item['main_branch_info']['latest_commit_time'] = response.css('relative-time::attr(datetime)').get()
+        
+        latest_commit_url = response.css('a[href*="/commit/"]::attr(href)').get()
+
+        self.logger.info('!!!!' * 10)
+        self.logger.info('INSIDE PARSE_COMMITS_PAGE')
+        yield response.follow(
+            latest_commit_url,
+                callback=self.parse_commit_message,
+                meta={
+                    'item': item,
+                    'releases_url': response.meta['releases_url']
+                })
+        
+    def parse_commit_message(self, response):
+        """Parse commit page for message."""
+        item = response.meta['item']
+        item['main_branch_info']['latest_commit_message'] = response.xpath('//div[@class="commit-title markdown-title"]//text()').getall()
+        self.logger.info('!!!!' * 10)
+        self.logger.info('INSIDE PARSE_COMMIT_MESSAGE')
+        yield response.follow(
+            response.meta['releases_url'],
+            callback=self.parse_releases_page,
+            meta={'item': item}
+        )
         
 
-    # def parse_releases_page(self, response):
-    #     """Parse releases page to extract data about the latest release."""
-    #     item = response.meta['item']
-    #     item['latest_release'] = dict()
-    #     # self.logger.debug(item)
-    #     releases = response.css('a::attr(href)').re('.*releases/tag.*')
-    #     item['latest_release'] = {}   
-    #     if releases:
-    #         latest_release_url = releases[0]
-    #         yield response.follow(latest_release_url, callback=self.parse_latest_release_info, meta={'item': item})
-    #     # else:
-    #         # yield item
+
+    def parse_releases_page(self, response):
+        """Parse releases page to extract data about the latest release."""
+        item = response.meta['item']
+        item['latest_release'] = {}
+        releases = response.css('a::attr(href)').re('.*releases/tag.*') 
+        self.logger.info('!!!!' * 10)
+        self.logger.info('INSIDE PARSE_RELEASES_PAGE')
+        if releases:        
+            self.logger.info(releases[0])
+        else:
+            self.logger.info('!!!!!!! NO RELEASES !!!!!!!')
+        if releases:
+            yield response.follow(
+                releases[0],
+                callback=self.parse_latest_release_info,
+                meta={'item': item})
+        else:
+            yield item
         
 
     
-    # def parse_latest_release_info(self, response):
-    #     """Parse info about the latest release."""
-    #     item = response.meta['item']
-    #     tag = response.xpath('//div[@data-test-selector="body-content"]//text()').getall()
-
-    #     changelog = response.xpath('//div[@data-test-selector="body-content"]//text()').getall()
-    #     item['latest_release']['changelog'] = changelog
-    #     # yield item
+    def parse_latest_release_info(self, response):
+        """Parse info about the latest release."""
+        item = response.meta['item']
+        tag = response.css('h1[class="d-inline mr-3"]::text').get()
+        item['latest_release']['tag'] = tag
+        # changelog = response.xpath('//div[@data-test-selector="body-content"]//text()').getall()
+        # item['latest_release']['changelog'] = changelog
+        
+        yield item
 
 
 
